71de2d39816d85fd6f3b6a0efb7ee6e472bda070
Boris Marković <bmarkovic17@outlook.com>
2025-05-08 14:09:50 +0200

    Add OpenApi and Scalar

--- Api/Program.cs
+++ Api/Program.cs
@@ -1,19 +1,31 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 using NewsApp.Api.Endpoints;
 using NewsApp.Api.Extensions;
+using Scalar.AspNetCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.AddNewsApp();
 
 builder.Services.AddOutputCache();
+builder.Services.AddOpenApi();
 
 var app = builder.Build();
 
 app.UseHttpsRedirection();
 app.UseAuthorization();
 app.UseOutputCache();
+
+if (app.Environment.IsDevelopment())
+{
+    app
+        .MapOpenApi()
+        .CacheOutput();
+
+    app.MapScalarApiReference();
+}
 
 app.MapEndpoints();
 
