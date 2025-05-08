095a5d267a285c992bbe7cc2bdc720ddc33934af
Boris Marković <bmarkovic17@outlook.com>
2025-05-08 06:50:29 +0200

    Update Result unit tests

--- UnitTests/Core/SharedKernel/ResultTests.cs
+++ UnitTests/Core/SharedKernel/ResultTests.cs
@@ -133,6 +133,7 @@ public class ResultTests
         // Assert
         Assert.True(result.IsSuccessful);
         Assert.Empty(result.Errors);
+        Assert.NotNull(result.Value);
     }
 
     [Fact]
@@ -151,5 +152,100 @@ public class ResultTests
         // Assert
         Assert.False(result.IsSuccessful);
         Assert.NotEmpty(result.Errors.SelectMany(error => error.Value));
+        Assert.Null(result.Value);
+    }
+
+    [Fact]
+    public void PagedGeneric_Success_NoErrors_ReturnsSuccessResult()
+    {
+        // Arrange
+
+        // Act
+        var result = PagedResult<object>.Success(new object(), page: 1, size: 10);
+
+        // Assert
+        Assert.True(result.IsSuccessful);
+        Assert.Empty(result.Errors);
+        Assert.NotNull(result.Value);
+        Assert.Equal(1, result.Page);
+        Assert.Equal(10, result.Size);
+    }
+
+    [Fact]
+    public void PagedGeneric_Fail_WithoutErrors_ReturnsFailResult()
+    {
+        // Arrange
+
+        // Act
+        var result = PagedResult<object>.Fail(new Dictionary<string, ICollection<string>>());
+
+        // Assert
+        Assert.False(result.IsSuccessful);
+        Assert.NotEmpty(result.Errors.SelectMany(error => error.Value));
+        Assert.Null(result.Value);
+        Assert.Null(result.Page);
+        Assert.Null(result.Size);
+    }
+
+    [Fact]
+    public void PagedGeneric_Fail_WithErrors_ReturnsFailResult()
+    {
+        // Arrange
+
+        // Act
+        var result = PagedResult<object>.Fail(new Dictionary<string, ICollection<string>>
+        {
+            [string.Empty] = ["error"]
+        });
+
+        // Assert
+        Assert.False(result.IsSuccessful);
+        Assert.NotEmpty(result.Errors.SelectMany(error => error.Value));
+        Assert.Null(result.Value);
+        Assert.Null(result.Page);
+        Assert.Null(result.Size);
+    }
+
+    [Fact]
+    public void PagedGeneric_Create_NoErrors_ReturnsSuccessResult()
+    {
+        // Arrange
+
+        // Act
+        var result = PagedResult<object>.Create(
+            valueFactory: () => new object(),
+            page : 1,
+            size: 10,
+            new Dictionary<string, ICollection<string>>());
+
+        // Assert
+        Assert.True(result.IsSuccessful);
+        Assert.Empty(result.Errors);
+        Assert.NotNull(result.Value);
+        Assert.Equal(1, result.Page);
+        Assert.Equal(10, result.Size);
+    }
+
+    [Fact]
+    public void PagedGeneric_Create_WithErrors_ReturnsFailResult()
+    {
+        // Arrange
+
+        // Act
+        var result = PagedResult<object>.Create(
+            valueFactory: () => new object(),
+            page: 1,
+            size: 10,
+            new Dictionary<string, ICollection<string>>
+            {
+                [string.Empty] = ["error"]
+            });
+
+        // Assert
+        Assert.False(result.IsSuccessful);
+        Assert.NotEmpty(result.Errors.SelectMany(error => error.Value));
+        Assert.Null(result.Value);
+        Assert.Null(result.Page);
+        Assert.Null(result.Size);
     }
 }
